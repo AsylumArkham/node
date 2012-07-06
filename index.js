bca9d6502d544e2832d3ee26fc833eeb6222a144
Jorge Ayuso <jl_ayuso@hotmail.com>
2012-07-06 15:25:17 -0600

    second commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var handle = {}
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
+handle["/show"] = requestHandlers.show;
 
 server.start(router.route, handle);
 
--- requestHandlers.js
+++ requestHandlers.js
@@ -1,22 +1,63 @@
-function start() {
+var querystring = require("querystring"), fs = require("fs"), formidable = require("formidable");
+
+function start(response, postData) {
     console.log("Request handler 'start' was called.");
     
-    function sleep(milliSeconds) {
-        var startTime = new Date().getTime();
-        while(new Date().getTime() < startTime + milliSeconds);
-    }
-    
-    sleep(3000);
-    
-    
-    return "Hello Start";
+    var body = '<html>'+ 
+        '<head>' +
+        '<meta http-equiv= "Content-Type" content="text/html; charset=UTF-8"/>' +
+        '</head>' +
+        '<body>' +
+        '<form action="/upload" enctype="multipart/form-data" method="post"> ' +
+        '<input type="file" name="upload" multiple="multiple">' +
+        '<input type="submit" value="Upload file" />'+
+        '</form>' +
+        '</body>' +
+        '</html>';
+        
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write(body);
+        response.end();
 }
 
-function upload() {
+function upload(response, request) {
     console.log("Request handler 'upload' was called.");
-    return "Hello Upload";
 
+    var form = new formidable.IncomingForm();
+    console.log("about to parse");
+  
+    form.parse(request, function(error, fields, files) {
+        console.log("parsing done");
+        fs.rename(files.upload.path, "c:/catwoman.png", function (err) {
+            if (err) {
+                fs.unlink("c:/catwoman.png");
+                fs.rename(files.upload.path, "c:/catwoman.bmp");
+            } 
+        }); 
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.write("received image: <br/>");
+        response.write("<img src='/show'/>");
+        response.end();
+    });
+  
+}
+
+function show(response) {
+    console.log("Request handler 'show' was called.");
+    fs.readFile("c:\\catwoman.bmp", "binary", function(error, file) {
+        if (error) {
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write(error + "\n");
+            response.end();
+        } else {
+            response.writeHead(200, {"Content-Type": "image/bmp"});
+            response.write(file, "binary");
+            response.end();
+        }
+    });
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
+
